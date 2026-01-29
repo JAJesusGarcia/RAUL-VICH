a0215a7aeffb386931402a86b3175e0dc0b235f8
Jesus <jesusjagarcia98@gmail.com>
2026-01-29 09:38:23 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { Navbar } from "@/src/components/layout/Navbar"
 import { VideoHero } from "@/src/components/media/VideoHero"
 import { BusExperience } from "@/src/components/sections/BusExperience"
 import { Destinations } from "@/src/components/sections/Destinations"
@@ -6,7 +5,6 @@ import { Destinations } from "@/src/components/sections/Destinations"
 export default function Home() {
   return (
     <>
-      {/* <Navbar /> */}
       <main className="pt-16">
         <VideoHero />
         <BusExperience />
