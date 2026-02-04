e38d4f9aad71d85af3ba50d5592236b22a6569ab
Jesus <jesusjagarcia98@gmail.com>
2026-02-04 10:00:17 -0300

    moví un componente a una carpeta

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 import { useEffect, useState } from "react"
 import { Menu, X } from "lucide-react"
 import { Button } from "@/src/components/ui/button"
-import { ThemeToggle } from "@/src/components/theme-toggle"
+import { ThemeToggle } from "@/src/components/ui/theme-toggle"
 
 
 export function Navbar() {
